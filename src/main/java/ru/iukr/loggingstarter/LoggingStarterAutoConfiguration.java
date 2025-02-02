316c4d711b97938ee6858cf80cd6aeb21e4423aa
Iulia N. Kriuchkova <leny_ostrich@mail.ru>
2025-02-02 04:02:10 +0300

    HW-10 Added aop

--- src/main/java/ru/iukr/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/iukr/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,7 +1,33 @@
 package ru.iukr.loggingstarter;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import ru.iukr.loggingstarter.aspect.LogExecutionAspect;
+import ru.iukr.loggingstarter.webfilter.WebLoggingFilter;
+import ru.iukr.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
+
+@AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
-    public static void println(String str) {
-        System.out.println("Вызвано из gradle: " + str);
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
+    public LogExecutionAspect logExecutionAspect() {
+        return new LogExecutionAspect();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true", matchIfMissing = true)
+    public WebLoggingFilter webLoggingFilter() {
+        return new WebLoggingFilter();
+    }
+
+    @Bean
+    @ConditionalOnBean(WebLoggingFilter.class)
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true")
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
+        return new WebLoggingRequestBodyAdvice();
     }
 }
